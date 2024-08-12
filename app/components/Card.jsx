8decc4fe592a7d52997c3ffa9dbaafb3d953cb2b
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-08-12 22:31:41 +0530

    Update Card.jsx

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -14,7 +14,7 @@ const Card = ({ img, head, information }) => {
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {head} </h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{information}</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-sky-400 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="https://github.com/adilsyed003" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-sky-400 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         View project
                     </a>
                 </div>
