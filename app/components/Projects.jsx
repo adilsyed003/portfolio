4f37794d42bd06d1a71b031364ecbac4ddbbc263
Adil abraar Syed <129985270+adilsyed003@users.noreply.github.com>
2024-08-26 23:31:41 +0530

    update

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -7,16 +7,21 @@ const Projects = () => {
                 img="project1.png"
                 head="Learners Way"
                 information="Learners Way is an innovative online platform developed using Next.js, offering a vast collection of free content aimed at helping developers enhance their skills. The platform features tutorials, and Road maps."
+                link="https://learners-way.vercel.app/"
             />
             <Card
                 img="project2.png"
                 head="Turbo - Typer"
                 information="Turbo - Typer is an engaging online game designed to help users improve their typing speed and accuracy. Built with React.js, the game provides real-time feedback."
+
+                link="https://turbo-typer.netlify.app/"
             />
             <Card
                 img="project3.png"
                 head="QR Generator"
                 information="QR Generator is a user-friendly platform that allows you to create custom QR codes effortlessly. By simply entering the URL, users can generate a QR code.It's a quick and efficient way to share web links."
+                link="https://github.com/adilsyed003"
+
             /></div>
     )
 }
