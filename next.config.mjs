0d978ec2ec1740c1c0d2b6f1101c33555540b2c1
Adil abraar Syed <129985270+adilsyed003@users.noreply.github.com>
2025-01-27 20:35:18 +0530

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio/',
+};
 
 export default nextConfig;
